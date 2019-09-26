aa6273e40357d8c9a205a0b8489eb8633632bfeb
Alexandre Silva <xande1231231@hotmail.com>
2019-09-26 19:16:16 -0300

    Fix layout

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,20 +1,21 @@
 var app = new Vue({
     el: "#app",
     data: {
-        menuLinksLeft: [{"name" : "Home", "icon" : "fas fa-home", "acessou" : true}, 
-        {"name" : "Explore", "icon" : "far fa-bookmark",  "acessou" : false},
-         {"name" : "Profile", "icon" : "fas fa-user",  "acessou" : false}],
-        where: {"name" : "Home", "icon" : "fas fa-home"},
-        user: {"name": "Alexandre Silva", "avatar": "https://pbs.twimg.com/profile_images/1064957428343681024/B3-Vw14W_normal.jpg"},
+        menuLinksLeft: [{ "name": "Home", "icon": "fas fa-home", "acessou": true },
+            { "name": "Explore", "icon": "far fa-bookmark", "acessou": false },
+            { "name": "Profile", "icon": "fas fa-user", "acessou": false }
+        ],
+        where: { "name": "Home", "icon": "fas fa-home" },
+        user: { "name": "Alexandre Silva", "avatar": "https://pbs.twimg.com/profile_images/1064957428343681024/B3-Vw14W_normal.jpg" },
         oqueQuero: "",
-        notifications : [
-        {"name" : "Home", "count" : 0},
-        {"name" : "Explore", "count" : 0},
-        {"name" : "Profile", "count" : 0},
-        {"name" : "Novo", "count" : 0}
-    ],
+        notifications: [
+            { "name": "Home", "count": 0 },
+            { "name": "Explore", "count": 0 },
+            { "name": "Profile", "count": 0 },
+            { "name": "Novo", "count": 0 }
+        ],
         logged: false,
-        games: [{"name" : "League Of Legends"}, {"name" : "Counter Strike Global Offensive"}, {"name" : "Outro"}],
+        games: [{ "name": "League Of Legends" }, { "name": "Counter Strike Global Offensive" }, { "name": "Outro" }],
         gameQueroA: "",
         outro: false,
         email: "",
@@ -23,8 +24,8 @@ var app = new Vue({
         tuturial: 0
     },
     methods: {
-        acessarLink(link){
-            for(var i = 0; i < this.menuLinksLeft.length; i++){
+        acessarLink(link) {
+            for (var i = 0; i < this.menuLinksLeft.length; i++) {
                 this.menuLinksLeft[i].acessou = false
             }
             const index = this.menuLinksLeft.indexOf(link)
@@ -33,43 +34,50 @@ var app = new Vue({
             this.menuLinksLeft[index] = link
             this.notifications[index].count = 0;
         },
-        oqueQueroA(oque){
+        oqueQueroA(oque) {
             this.oqueQuero = oque
         },
-        qualGame(game){
+        qualGame(game) {
             this.gameQueroA = game
-            if(game.name == "Outro"){
+            if (game.name == "Outro") {
                 this.outro = true
                 this.gameQueroA = ""
-            } else{
+            } else {
                 this.outro = false
                 this.gameQueroA = game.name
             }
         },
-        cadastrar(){
+        cadastrar() {
             this.logged = true
             this.user.name = this.nome
-            if(this.oqueQuero == 'ensinar'){
-                var novaArea = {"name" : "Ensinar", "icon" : "fas fa-chalkboard-teacher", "acessou" : false}
+            if (this.oqueQuero == 'ensinar') {
+                var novaArea = { "name": "Ensinar", "icon": "fas fa-chalkboard-teacher", "acessou": false }
                 this.menuLinksLeft.push(novaArea)
-            } else if(this.oqueQuero == 'aprender'){
-                var novaArea = {"name" : "Aprender", "icon" : "fas fa-school", "acessou" : false}
+                this.scrollToTop()
+            } else if (this.oqueQuero == 'aprender') {
+                var novaArea = { "name": "Aprender", "icon": "fas fa-school", "acessou": false }
                 this.menuLinksLeft.push(novaArea)
+                this.scrollToTop()
             }
         },
-        tuto(){
-            if(this.tuturial == 0){
+        tuto() {
+            this.scrollToTop()
+            if (this.tuturial == 0) {
                 this.tuturial = 1
-            } else if(this.tuturial == 1){
+            } else if (this.tuturial == 1) {
                 this.tuturial = 2;
-                for(var i = 0; i < this.menuLinksLeft.length; i++){
+                for (var i = 0; i < this.menuLinksLeft.length; i++) {
                     this.menuLinksLeft[i].acessou = false
                 }
-                this.where = {"name" : "Explore", "icon" : "far fa-bookmark",  "acessou" : true}
-                this.menuLinksLeft[1] = {"name" : "Explore", "icon" : "far fa-bookmark",  "acessou" : true}
-            } else{
+                this.where = { "name": "Explore", "icon": "far fa-bookmark", "acessou": true }
+                this.menuLinksLeft[1] = { "name": "Explore", "icon": "far fa-bookmark", "acessou": true }
+            } else {
                 this.tuturial = 3
             }
+        },
+        scrollToTop() {
+            var body = document.body; // Safari
+            body.scrollTop = 0;
         }
     }
 });
